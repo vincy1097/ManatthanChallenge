48e05c536c2e9b66f5c61025f2515a8755cc8e69
Vincenzo Izzo <vincenzoizzo@macbook-pro-di-vincenzo-3.local>
2017-12-13 16:07:17 +0100

    Initial Commit

---
+++ ManatthanChallenge/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ManatthanChallenge
+//
+//  Created by Vincenzo Izzo on 13/12/17.
+//  Copyright © 2017 Vincenzo Izzo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ManatthanChallenge/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ManatthanChallenge
+//
+//  Created by Vincenzo Izzo on 13/12/17.
+//  Copyright © 2017 Vincenzo Izzo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
